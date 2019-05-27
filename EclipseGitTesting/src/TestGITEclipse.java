9ffc28ca3f67ac7a257bf092bab22639aeccb88b
Kiranlc <51041857+Kiranlc@users.noreply.github.com>
2019-05-27 22:41:50 +0530

    Update TestGITEclipse.java

--- EclipseGitTesting/src/TestGITEclipse.java
+++ EclipseGitTesting/src/TestGITEclipse.java
@@ -4,6 +4,7 @@ public class TestGITEclipse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Test GIT");
+		System.out.println("Test GIT123");
 	}
 
 }
